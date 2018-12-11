8f645004f28775c224b544971b3e86fa67ed1327
JuaniBrunero <juanibrunero@gmail.com>
2018-12-11 16:35:52 -0300

    Buscaminas listo

--- buscaminas/buscaminas.py
+++ buscaminas/buscaminas.py
@@ -1,4 +1,4 @@
-import pygame
+import curses
 import random
 
 class Mina:
@@ -7,7 +7,7 @@ class Mina:
     def __init__(self):
         return
 
-    def clicked(m, x, y):
+    def clicked(self, m, x, y):
         fin()
 
 class Numero:
@@ -18,9 +18,9 @@ class Numero:
         pass
 
     def clicked(self, m, x, y):
-        if self.cant == 0:
-            mirarMas(m, x, y)
         self.show = True
+        if self.cant == '-':
+            mirarMas(m, x, y)
 
 def llenarTablero(M, f, c, m):
     for i in range(0, m):
@@ -46,7 +46,27 @@ def contar(m, x, y):
             elif -1 < (x + i) < f and -1 < (y + j) < c:
                 if isinstance(m[x+i][y+j], Mina):
                     s = s+1
+    if s == 0:
+        s = '-'
     return s
+
+def mirarMas(m, x, y):
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == 0 and j == 0:
+                continue
+            elif -1 < (x + i) < alto and -1 < (y + j) < ancho:
+                if not m[x+i][y+j].show and isinstance(m[x+i][y+j], Numero):
+                    m[x+i][y+j].clicked(m, x+i, y+j)
+
+def fin():
+    for x in range(0, ancho):
+        for y in range(0, alto):
+            win.addch(y, 2*x, str(matrix[x][y].cant), curses.color_pair(3))
+    while True:
+        key = win.getch()
+        if key == 27:
+            quit()
 
 def mostrar(m, f, c):
     for i in range(f):
@@ -55,14 +75,55 @@ def mostrar(m, f, c):
             S = S + str(m[i][j].cant) + "  "
         print(S)
 
-f = 4
-c = 4
-w = 30
+curses.initscr()
+curses.start_color()
+curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)
+curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
+curses.init_pair(4, curses.COLOR_GREEN, curses.COLOR_BLACK)
+curses.curs_set(0)
 
-minas = 3
+ancho = 20
+alto = 20
 
-matrix = [[x for x in range(c)] for y in range(f)]
+minas = 30
 
-llenarTablero(matrix, f, c, minas)
+win = curses.newwin(alto, 2*ancho, 0, 0)
+win.keypad(1)
+curses.mousemask(curses.ALL_MOUSE_EVENTS)
 
-mostrar(matrix, f, c)
+matrix = [[x for x in range(ancho)] for y in range(alto)]
+
+llenarTablero(matrix, alto, ancho, minas)
+
+for x in range(0, ancho):
+    for y in range(0, alto):
+        #win.addch(y, 2*x, str(matrix[x][y].cant))
+        win.addch(y, 2*x, 'X', curses.color_pair(1))
+
+while True:
+    key = win.getch()
+
+    if key == 27:
+        quit()
+
+    if key == curses.KEY_MOUSE:
+        _, mx, my, _, b = curses.getmouse()
+        x = mx/2
+        y = my
+        mx = mx - (mx % 2)
+        if -1 < y < alto and -1 < mx < 2*ancho :
+            #Derecho
+            if b > 1023:
+                win.addch(my, mx, 'D')
+            #Izquiedo
+            if b < 1024:
+                matrix[x][y].clicked(matrix, x, y)
+                win.addch(my, mx, 'I')
+
+            for x in range(0, ancho):
+                for y in range(0, alto):
+                    if matrix[x][y].show:
+                        win.addch(y, 2*x, str(matrix[x][y].cant), curses.color_pair(2))
+                    else:
+                        win.addch(y, 2*x, 'X', curses.color_pair(1))
