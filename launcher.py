35d1c144dc558377193919b424b427781647dbea
Alejandro Bullrich <alejandro.bullrich@mbp-alebullrich.local>
2018-12-14 15:43:37 -0300

    Adds mac support and space-war

--- launcher.py
+++ launcher.py
@@ -1,5 +1,6 @@
 import subprocess
 import curses
+import os
 
 s = curses.initscr()
 curses.start_color()
@@ -14,20 +15,22 @@ snake          = "snake/snake.py"
 buscaminas     = "buscaminas/buscaminas.py"
 tetris         = "tetris/tetris.py"
 space_invaders = "space_invaders/space_invaders.py"
+space_war = "space_war/space_war.py"
 
-listaJuegos = [snake, buscaminas, tetris, space_invaders]
+listaJuegos = [snake, buscaminas, tetris, space_invaders, space_war]
 
 win = curses.newwin(alto, ancho, 0, 0)
 curses.mousemask(curses.ALL_MOUSE_EVENTS)
 win.keypad(1)
 win.border('|', '|', '+', '+', '.', '.')
 
-win.addstr(2 , ancho/2 - 10, "GAME ARCADE", curses.color_pair(1))
+win.addstr(2, ancho/2 - 10, "GAME ARCADE", curses.color_pair(1))
 
 win.addstr(8, 2, "Snake"          , curses.color_pair(2))
-win.addstr(12, 2, "Buscaminas"    , curses.color_pair(2))
-win.addstr(16, 2, "Tetris"        , curses.color_pair(2))
-win.addstr(20, 2, "Space Invaders", curses.color_pair(2))
+win.addstr(10, 2, "Buscaminas"    , curses.color_pair(2))
+win.addstr(12, 2, "Tetris"        , curses.color_pair(2))
+win.addstr(14, 2, "Space Invaders", curses.color_pair(2))
+win.addstr(16, 2, "Space War     ", curses.color_pair(2))
 
 
 while True:
@@ -45,4 +48,10 @@ while True:
             if b < 1024:
                 for i in range(0, len(listaJuegos)):
                     if 6+4*i < my < 10+4*i:
-                        subprocess.Popen("python " + listaJuegos[i], creationflags=subprocess.CREATE_NEW_CONSOLE)
+                        if os.name == 'posix':
+                            cwd = os.getcwd()
+                            os.system(
+                                "osascript -e 'tell application \"Terminal\" to do script \"cd {0} && python2.7 {1}\" activate'".format(
+                                    cwd, listaJuegos[i]))
+                        else:
+                            subprocess.Popen("python " + listaJuegos[i], creationflags=subprocess.CREATE_NEW_CONSOLE)
